6f8d4f152cc7557e3bb951c0066fd17e7f99bd60
liu149 <liuqi1212@outlook.com>
2020-03-03 19:00:09 +0800

    first commit spring-framework

---
+++ test/src/main/java/com/lq/spring/config/AppConfig.java
@@ -0,0 +1,10 @@
+package com.lq.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.lq.spring")
+public class AppConfig {
+
+}
